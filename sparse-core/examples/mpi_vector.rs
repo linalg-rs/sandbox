2c344257bb1556fc3df814e297db6ed612e23395
Timo Betcke <timo.betcke@gmail.com>
2023-01-31 19:08:43 +0000

    Cleaned up example.

--- sparse-core/examples/mpi_vector.rs
+++ sparse-core/examples/mpi_vector.rs
@@ -5,7 +5,6 @@ use sparse_core::distributed::index_layout::DistributedIndexLayout;
 use sparse_core::distributed::indexable_space::DistributedIndexableVectorSpace;
 use sparse_core::local::index_layout::LocalIndexLayout;
 use sparse_core::local::indexable_vector::LocalIndexableVector;
-use sparse_traits::IndexLayout;
 use sparse_traits::linalg::*;
 use sparse_traits::Element;
 use sparse_traits::LinearSpace;
@@ -17,17 +16,12 @@ fn main() {
 
     let n = 100;
 
+    // We first create an index layout.
     let index_layout = DistributedIndexLayout::new((0, n), &world);
-
     let space = DistributedIndexableVectorSpace::<'_, f64, _>::new(&index_layout);
-
     let mut vec = space.create_element();
 
     let vec_impl = vec.view_mut();
-
-    // if let Some(val) = vec_impl.view_mut().unwrap().get_mut(0) {
-    //     *val = 0.5;
-    // }
 
     let mut local_vec: LocalIndexableVector<'_, f64>;
 
@@ -42,28 +36,18 @@ fn main() {
         }
 
         println!("Local inf norm: {}", local_vec.norm_inf());
+        println!("Local inner: {}", local_vec.inner(&local_vec).unwrap());
 
         let _ = vec_impl.fill_from_root(&Some(local_vec));
-
-        println!("Root: {}", vec_impl.view().unwrap().data().get(49).unwrap());
-        println!("Global dofs {}", vec_impl.index_layout().number_of_global_indices());
-        println!("Dofs at root {:#?}", vec_impl.index_layout().index_range(1));
     } else {
         let _ = vec_impl.fill_from_root(&None);
-        println!("Proc 1: {}", vec_impl.view().unwrap().data().get(49).unwrap());
-        println!("Dofs at root {:#?}", vec_impl.index_layout().index_range(1));
     }
-    println!("Inner: {}", vec.view().inner(&vec.view()).unwrap());
-    println!("Inf norm: {}", vec.view().norm_inf());
 
-    if world.rank() == 0 {
-        // for index in 0..(world.size() as usize) {
-        //     println!(
-        //         "count: {}, displacement: {}",
-        //         index_layout.counts()[index],
-        //         index_layout.displacements()[index]
-        //     );
-        // }
-        // println!("Square sum: {}", vec.view().abs_square_sum());
+    let inner = vec.view().inner(&vec.view()).unwrap();
+    let inf_norm = vec.view().norm_inf();
+
+    if rank == 0 {
+        println!("Inner: {}", &inner);
+        println!("Inf norm: {}", &inf_norm);
     }
 }
